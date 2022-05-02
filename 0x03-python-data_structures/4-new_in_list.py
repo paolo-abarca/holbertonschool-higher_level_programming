3c64076d888f119a613c805767504070e88b6583
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-02 12:41:30 -0500

    Updating python script that replaces an element in a list at a specific position without modifying the original list (like in C)

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copy_list = my_list.copy()
     if idx < 0:
         return copy_list
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return copy_list
     copy_list[idx] = element
     return copy_list
