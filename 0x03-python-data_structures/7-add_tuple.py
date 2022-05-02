b13fa95d0ba3ba6257486ccbbec5821bc106ec4e
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-02 14:05:49 -0500

    Fixing python script that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b + (0, 0)
 
     elif len(tuple_a) > 2 and len(tuple_b) > 2:
-        result = (tuple_a[0] + tuple_b[0])
+        result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
         return result
 
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
