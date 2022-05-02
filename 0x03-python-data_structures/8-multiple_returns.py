76e2e19a7167ac30eedde5f0f19cad9ad2d62879
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-02 14:58:39 -0500

    Fixing python script that returns a tuple with the length of a string and its first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     first_char = sentence[0]
 
     if lon == 0:
-        sentence[0] is None
+        return (0, None)
     else:
         _tuple = (lon, first_char)
         return _tuple
