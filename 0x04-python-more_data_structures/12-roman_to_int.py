8ec4dc24d511aa64f79174a1567bfcbf9a218f8b
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-04 21:35:51 -0500

    Updating a python script that converts a roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
     for i in range(len(roman_string)):
@@ -10,4 +11,5 @@ def roman_to_int(roman_string):
                     * rom_val[roman_string[i - 1]]
         else:
             int_val += rom_val[roman_string[i]]
+
     return int_val
