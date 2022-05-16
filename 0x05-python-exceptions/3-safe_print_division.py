82211513c94f62402f874eb7df49849755be9edb
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-16 13:12:51 -0500

    Updating python script that divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        return a / b
+        result = a / b
     except ZeroDivisionError:
-        return None
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
