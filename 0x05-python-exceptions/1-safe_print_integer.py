11ecd66d3161044ac4608b524cbae751a83d6a52
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-16 12:31:57 -0500

    Fixing Python script that prints an integer with {:d}.format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-
-    except ValueError:
+        return True
+    except Exception:
         return False
-    return True
