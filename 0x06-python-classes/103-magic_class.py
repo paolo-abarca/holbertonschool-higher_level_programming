19c7e79f29e146b99fc006d68eba07becd2bdd09
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-17 20:25:28 -0500

    Fixing python script that does exactly the same thing as the following python bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,10 +9,13 @@ class MagicClass:
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-            self._MagicClass__radius = radius
+        self._MagicClass__radius = radius
 
     def area(self):
         return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
         return 2 * math.pi * self._MagicClass__radius
+
+import dis
+dis.dis(MagicClass)
