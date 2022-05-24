8ad8c5124223789c250ee98dd4659ad56e66aec0
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-24 16:33:33 -0500

    Adding Python script that prints My name is <firstname> <lastname>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python function
+that prints my name is
+first name and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print string:
+    first name and last name
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
