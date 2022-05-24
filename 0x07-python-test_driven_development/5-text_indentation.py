6c2ed86306f38146d3b710a889f39f8db7d75e7b
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-24 16:38:07 -0500

    Adding a python script that prints a text with 2 newlines after each of these characters ., ? Y :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Python function
+that prints a text
+with 2 new lines
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text
+    with 2 new lines
+    """
+
+    characters = [".", "?", ":"]
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in characters:
+            print("\n")
+
+            try:
+                if text[i + 1] == " ":
+                    i += 1
+            except IndexError:
+                break
+
+        i += 1
