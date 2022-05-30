8c009d36b33108f9bc6673dcad47397d129e67dc
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-30 18:32:01 -0500

    Fixing Python script that returns True if the object is an instance of a class it inherited (directly or indirectly), False otherwise

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,8 @@ from the specified class ; otherwise False
 def inherits_from(obj, a_class):
     """
     returns True if the object is an instance
-    of a class that inherited, otherwise False
+    of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
