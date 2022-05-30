9d4d22dd2358fb8e6751539fc67b501aa642f8ad
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-30 18:57:20 -0500

    Adding a Python script that creates a BaseGeometry class (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        """
+        area method that prints an exception message
+        """
+        raise Exception("area() is not implemented")
