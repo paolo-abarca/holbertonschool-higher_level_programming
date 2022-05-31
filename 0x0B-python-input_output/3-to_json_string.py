eae4f98694b8dff536aa81f1d1c3ed1d8eadfc8c
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-31 18:33:08 -0500

    Adding a Python script that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a function that returns the
+JSON representation of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Adding a Python script that returns the
+    JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
