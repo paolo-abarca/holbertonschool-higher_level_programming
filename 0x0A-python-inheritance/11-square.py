d2d860e8c4c34cbe31ae1b00afb69c53ca22eed7
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-30 20:23:21 -0500

    Adding a Python script that writes a Square class that inherits from Rectangle (9-rectangle.py) and area method and str method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Write a class Square
+(based on 9-rectangle.py)
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square inherits
+    from Rectangle
+    (9-rectangle.py)
+    """
+
+    def __init__(self, size):
+        """
+        instances initializes
+        self, size
+        """
+
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        """
+        str method that returns a message
+        """
+
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """
+        area method that returns the area of a square
+        """
+
+        return self.__size ** 2
