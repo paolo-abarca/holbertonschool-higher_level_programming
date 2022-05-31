b23ab67450e8a3aad735a70be83e8ab156baf421
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-31 18:38:24 -0500

    Adding Python script that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that returns an object
+(Python data structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    method that returns an object
+    """
+    return json.loads(my_str)
