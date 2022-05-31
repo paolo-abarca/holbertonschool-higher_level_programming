5674abb2b6010db27a9e8caf22a9cbbe9454bfbd
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-30 19:27:30 -0500

    Updating a Python script that creates a Rectangle class that inherits from BaseGeometry (7-base_geometry.py)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle(BaseGeometry):
         self, width, height
         """
 
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
