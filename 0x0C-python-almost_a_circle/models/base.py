eec34b3a9fdfe07649b6f3a93120be585fcc6b1c
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-06-06 22:55:38 -0500

    updating the base.py class file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ of all other classes in this project.
 """
 import json
 import os
+import csv
 
 
 class Base:
@@ -108,3 +109,58 @@ class Base:
 
         else:
             return list_
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        class method save_to_file_csv(cls, list_objs):
+        which writes the JSON string representation of
+        list_objs to a file
+        """
+        filename = cls.__name__ + ".csv"
+        string = "[]"
+        with open(filename, "w") as f:
+            if list_objs is None or list_objs == []:
+                f.write(string)
+
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+
+                if cls.__name__ == "Square":
+                    fieldnames = ["id", "size", "x", "y"]
+
+                write = csv.DictWriter(f, fieldnames=fieldnames)
+
+                for i in list_objs:
+                    write.writerow(i.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        class method load_from_file_csv(cls):
+        that serializes and deserializes in CSV
+        """
+        filename = cls.__name__ + ".csv"
+        string = []
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                read = csv.reader(f, delimiter=',')
+
+                if cls.__name__ == "Rectangle":
+                    field = ["id", "width", "height", "x", "y"]
+
+                if cls.__name__ == "Square":
+                    field = ["id", "size", "x", "y"]
+
+                for j, k in enumerate(read):
+                    if j > 0:
+                        i = cls(1, 1)
+
+                        for o, m in enumerate(k):
+                            if m:
+                                setattr(i, field[o], int(m))
+
+                        string.append(i)
+
+        return string
