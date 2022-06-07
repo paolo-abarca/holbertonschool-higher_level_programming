ed4fae89d0396ea9c3947b3aebd18fdf8a68f34c
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-06-06 23:06:13 -0500

    fixing the base.py class file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -113,54 +113,30 @@ class Base:
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """
-        class method save_to_file_csv(cls, list_objs):
-        which writes the JSON string representation of
-        list_objs to a file
+        JSON string representation of list_objs in a file
         """
+        new_list = []
+        if list_objs:
+            for i in list_objs:
+                dictionary = i.to_dictionary()
+                new_list.append(dictionary)
         filename = cls.__name__ + ".csv"
-        string = "[]"
-        with open(filename, "w") as f:
-            if list_objs is None or list_objs == []:
-                f.write(string)
-
-            else:
-                if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-
-                if cls.__name__ == "Square":
-                    fieldnames = ["id", "size", "x", "y"]
-
-                write = csv.DictWriter(f, fieldnames=fieldnames)
-
-                for i in list_objs:
-                    write.writerow(i.to_dictionary())
+        with open(filename, mode="w", encoding="utf-8")as myFile:
+            myFile.write(cls.to_json_string(new_list))
 
     @classmethod
     def load_from_file_csv(cls):
         """
-        class method load_from_file_csv(cls):
-        that serializes and deserializes in CSV
+        returns a list of instances
         """
         filename = cls.__name__ + ".csv"
-        string = []
-        if os.path.exists(filename):
-            with open(filename, "r") as f:
-                read = csv.reader(f, delimiter=',')
-
-                if cls.__name__ == "Rectangle":
-                    field = ["id", "width", "height", "x", "y"]
-
-                if cls.__name__ == "Square":
-                    field = ["id", "size", "x", "y"]
-
-                for j, k in enumerate(read):
-                    if j > 0:
-                        i = cls(1, 1)
-
-                        for o, m in enumerate(k):
-                            if m:
-                                setattr(i, field[o], int(m))
-
-                        string.append(i)
-
-        return string
+        try:
+            with open(filename, mode="r", encoding="utf-8") as myFile:
+                readFile = myFile.read()
+                list_dictionary = cls.from_json_string(readFile)
+                new = []
+                for items in list_dictionary:
+                    new.append(cls.create(**items))
+                return new
+        except Exception:
+            return []
