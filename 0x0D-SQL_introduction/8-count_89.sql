92043c04963d6aacf0fbeec28775d54193f9b527
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-05 17:55:05 -0700

    Adding script that displays the number of records with id = 89 in the first_table table of the hbtn_0c_0 database on your MySQL server

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- script that displays the number of records with id = 89 --
+-- in the first_table table of the hbtn_0c_0 database on your MySQL server --
+SELECT count(*) FROM first_table WHERE id = 89;
