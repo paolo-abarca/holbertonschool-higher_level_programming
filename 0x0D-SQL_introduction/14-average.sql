323a5a99b10cb4e47851337486f2e497a283a759
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-07-05 20:07:33 -0700

    Adding script that calculates the average score of all the records in the second_table table of the hbtn_0c_0 database on your MySQL server

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- script that calculates the average score of all the records in the --
+-- second_table table of the hbtn_0c_0 database on your MySQL server --
+SELECT AVG(score) "average" FROM second_table;
