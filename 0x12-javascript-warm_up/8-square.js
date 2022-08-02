1f37d844a5c6d6e57f2471bfd16a0e46ac9785ea
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-02 12:11:19 -0500

    Adding Javascript script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const size = process.argv[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= size; i++) {
+    let row = '';
+
+    for (let j = 1; j <= size; j++) {
+      row = row + 'X';
+    }
+    console.log(row);
+  }
+}
