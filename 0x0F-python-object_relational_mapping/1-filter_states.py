ced68c2611324537cd1da2d4dd50472aee4a26f9
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-06 15:17:04 -0700

    Updating a python script that lists all states with a name starting with N (top N) from the hbtn_0e_0_usa database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name \
-            LIKE 'N%' ORDER BY states.id")
+            LIKE BINARY 'N%' ORDER BY states.id")
 
     rows = cursor.fetchall()
     for row in rows:
