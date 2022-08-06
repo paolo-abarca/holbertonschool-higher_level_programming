f2709a37199702fb9592dd90e55c0b27695c1f0a
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-06 15:14:23 -0700

    Adding a python script that lists all states with a name starting with N (top N) from the hbtn_0e_0_usa database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+python script that lists all states with a name
+starting with N (top N) from the hbtn_0e_0_usa database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                         db=argv[3], host='localhost', port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name \
+            LIKE 'N%' ORDER BY states.id")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
