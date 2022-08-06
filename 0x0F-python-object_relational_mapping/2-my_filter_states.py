da9591273af9ea8cfdc66270279bee4bc5fbeebc
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-06 16:29:30 -0700

    Fixing a python script that takes an argument and displays all values in the state table of hbtn_0e_0_usa where the name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,19 +4,19 @@ Adding a python script that takes an argument
 and displays all values in the state table of
 hbtn_0e_0_usa where the name matches the argument
 """
-import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                         db=argv[3], host='localhost', port=3306)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name \
-            LIKE BINARY '" + argv[4] + "' ORDER BY states.id")
-
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name \
+            LIKE BINARY '{}' ORDER BY id".format(sys.argv[4]))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
