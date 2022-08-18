c7503bd60e8d83c0a243b443e3a5d1cb1e347cd6
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-18 13:13:11 -0500

    Fixing Bash script that takes a URL, sends a request to that URL, and returns the size of the response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes a URL, sends a request to that URL, and returns the size of the response body
-curl -sI 0.0.0.0:5000 | grep -i Content-Length | cut -d " " -f2
+curl -sI $1 | grep -i Content-Length | cut -d " " -f2
