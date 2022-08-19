288ada103ff1cb6289ebfb000309833eb747f4f6
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-19 13:32:14 -0500

    Adding Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    readpage = response.read()
+
+content = str(readpage).split("\'")
+
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+      .format(type(readpage), readpage, content[1]))
