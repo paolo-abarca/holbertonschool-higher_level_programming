3c5869a1d013f7200056d6fd87f1e037d2e85c20
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-19 20:14:00 -0500

    Adding python script that takes 2 arguments to solve this challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     r = requests.get("https://api.github.com/repos/{}/{}/commits"
                      .format(argv[1], argv[2]))
 
-    if r.status_code >= 400:
-        print("None")
+    fjson = r.json()
 
-    else:
-        fjson = r.json()
+    try:
         for i in range(10):
             print("{}: {}".format(fjson[i].get('sha'),
                                   fjson[i].get('commit').get('author')
                                   .get('name')))
+    except IndexError:
+        pass
