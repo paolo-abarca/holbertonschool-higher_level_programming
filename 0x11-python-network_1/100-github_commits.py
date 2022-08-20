c1e32c7bbf296af0ff0ee072128707702e27ba88
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-08-19 21:39:29 -0500

    Fixing python script that takes 2 arguments to solve this challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
 
     r = requests.get(url)
 
