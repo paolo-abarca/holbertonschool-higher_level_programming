c1bb22d9c9bc809fe4a73362efc95c37dd5801d8
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-13 14:19:44 -0500

    Adding script that display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+axios.get(process.argv[2]).then(
+  resp => {
+    console.log('code: ' + resp.status);
+  },
+  err => {
+    console.log('code: ' + err.response.status);
+  });
