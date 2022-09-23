5759c82561cbc4aeb2898e1ad4c72a08ac31004f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-23 11:19:15 -0500

    Adding script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$('INPUT#btn_translate').click(function () {
+  const value = $('INPUT#language_code').val();
+  const url = 'https://stefanbohacek.com/hellosalut/?lang=' + value;
+  $.get(url, function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
+$('INPUT#language_code').keypress(function (e) {
+  if (e.which === 13) {
+    const value = $('INPUT#language_code').val();
+    const url = 'https://stefanbohacek.com/hellosalut/?lang=' + value;
+    $.get(url, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  }
+});
